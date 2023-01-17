9a31014fcbb4081d9c20e75e334582b55727a107
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-01-17 17:26:21 +0200

    //

--- src/index.js
+++ src/index.js
@@ -65,41 +65,6 @@ function loadMore() {
   });
 }
 
-// function renderGallery(arr) {
-//   const markup = arr
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `<div class="photo-card">
-//       <a href="${largeImageURL}">
-//       <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
-//       </a>
-//   <div class="info">
-//       <p class="info-item"><b>Likes</b>
-//         ${likes}
-//       </p>
-//       <p class="info-item"><b>Views</b>
-//         ${views}
-//       </p>
-//       <p class="info-item"><b>Comments</b>
-//         ${comments}
-//       </p>
-//       <p class="info-item"><b>Downloads</b>
-//         ${downloads}
-//     </div>
-// </div>`
-//     )
-//     .join('');
-
-//   // refs.galleryRef.insertAdjacentHTML('beforeend', markup);
-// }
-
 function clearInnerHTML() {
   refs.galleryRef.innerHTML = '';
 }
